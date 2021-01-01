fd2471c6e09be4ad0426eee1bbb5fa45f420725b
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2021-01-01 14:57:26 -0800

    lab-07 done

--- README.md
+++ README.md
@@ -1,8 +1,53 @@
 # city_explorer_api.
 # lab-06-Backend
 
+ # Project Name 
+ # city_explorer_api.
 
-pk.436b00e27289c302066606840ecd3bbd     location
+**Author**: Nassir Abegaz
+**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
 
-7cba562bf707483cb7dc64e1fdad65a1    weather
-GEOCODE_API_KEY=436b00e27289c302066606840ecd3bbd 
+## Overview
+ what this application is and why you are building it, beyond the fact that it's an assignment for this class. 
+
+ This application about providing information(data) for the client when its requested(search) and the data a the first time will go to place contain the data by using API key .
+## Getting Started
+creating Repo
+creating server.js
+find API keys for location & weather.
+
+## Architecture
+to built this apps i used 
+
+dotenv
+express
+cors
+superagent
+locationiq to get API
+weatherbit  to get API
+
+
+## Change Log
+
+Number and name of feature: _2__
+location and weather API
+
+Estimate of time needed to complete: __4HOURS___
+
+Start time: _8PM ____
+
+Finish time: _____
+
+Actual time needed to complete: _5 HOURS____
+
+
+## Credits and Collaborations
+
+TO print out the validate date and weather URL Nicholas Ryan(TA'S)
+
+
+
+
+
+GEOCODE_API_KEY=pk.436b00e27289c302066606840ecd3bbd
+EATHER_API_KEY=7cba562bf707483cb7dc64e1fdad65a1
--- server.js
+++ server.js
@@ -1,71 +1,90 @@
 'use strict';
-//declare application dependencies
+
+//declare the application dependencies/ bring in my dependencies(modules)
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const superagent = require('superagent');
+
+
+// start my application
+
 const app = express();
 app.use(cors());
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3002;
 
-
-app.get('/',(request, response)=>{
-  response.send('hello World');
+// Routes
+app.get('/', (request, response) => {
+  response.status(200).send('hello World');
 });
 app.get('/location', locationHandler);
-// app.get('/weather', weatherHandler);
-// app.use('*', errorHandler);
+app.get('/weather', weatherHandler);
 
-function locationHandler(request, response){
-  const geoData = require('./data/location.json');
-  const city = request.query.city;
-  const locationData = new Location(city, geoData);
+function locationHandler(request, response) {
+  const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+  let city = request.query.city;
+  const url = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${request.query.city}&format=json`;
+  //a-snsicrness request
+  superagent.get(url)
+    // then response data
+    .then(data => {
+      const locationData = (data.body[0]);
+      //  create our objects based on our constructor
+      const location = new Location(city, locationData);
+      response.status(200).send(location);
 
-  response.send(locationData);
+    });
 }
 
+function weatherHandler(request, response) {
+  // const city = request.query.search_query;
+  const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+  const longitude = request.query.longitude;
+  const latitude = request.query.latitude;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&lat=${request.query.latitude}&lon=${request.query.longitude}&days=8`;
 
+  superagent.get(url).then(instantWeather => {
+    const weatherForecast = instantWeather.body;
+    const weatherData = weatherForecast.data.map(instantWeather =>
+      new Weather(instantWeather));
+
+
+
+      response.send(weatherData);
+  
+  }).catch(error => console.log(error));
+
+}
 
 // // constructor function
 
 function Location(city, geoData) {
   this.search_query = city;
-  this.formatted_query = geoData[0].display_name;
-  this.latitude = geoData[0].lat;
-  this.longitude = geoData[0].lon;
+  this.formatted_query = geoData.display_name;
+  this.latitude = geoData.lat;
+  this.longitude = geoData.lon;
 }
 
 
-function Weather(weather){
-  // this.time = weather.valid_date;
-  // this.forecast =weather.weather.description;
+function Weather(weather) {
   this.forecast = weather.weather.description;
   let date = Date.parse(weather.datetime);
   this.time = new Date(date).toDateString();
 
 }
-app.get('/weather',(request, response)=>{
-  let emptyArr = [];
-  const weatherData = require('./data/weather.json');
-  weatherData.data.forEach(weatherData=>{
-    const updatedWeather = new Weather(weatherData);
-    emptyArr.push(updatedWeather);
 
-  });
-  // response.status(200).json(emptyArr);
-  response.send(emptyArr);
 
-});
-
-app.use('*', errorHandler);
-
-function errorHandler(request, response){
+function errorHandler(request, response) {
   response.status(500).send('Sorry, something went wrong');
 }
+app.use('*', errorHandler);
 
+app.listen(PORT, () => console.log(`app is listining ${PORT}`));
 
 app.use('*', (request, response) => {
   response.status(500).send('Sorry, something went wrong');
 });
-app.listen(PORT, () => console.log(`app is listining ${PORT}`));
+
+// two paramater listing port and ()callback function
 
 
